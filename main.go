12105503d659c468595c10994e6e85ad05204f0b
Raphael Oliveira <raphaeldias.ti@gmail.com>
2023-08-01 23:18:43 -0300

    feat: improvements

--- main.go
+++ main.go
@@ -10,12 +10,31 @@ import (
 
 func main() {
 	router := gin.Default()
+	router.SetTrustedProxies(nil)
+
 	router.GET("/questions", getQuestions)
+	router.POST("/quiz", postQuiz)
 
 	router.Run("localhost:8080")
 }
 
-// getAlbums responds with the list of all albums as JSON.
 func getQuestions(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, service.GetAll())
+}
+
+func postQuiz(c *gin.Context) {
+	var answers map[string]string
+
+	if err := c.BindJSON(&answers); err != nil {
+		return
+	}
+
+	user := c.GetHeader("x-user")
+	if user == "" {
+		c.String(http.StatusBadRequest, "Header x-user not informed")
+		return
+	}
+
+	service.Create(user, answers)
+	c.IndentedJSON(http.StatusCreated, answers)
 }
--- questions/service.go
+++ questions/service.go
@@ -1,5 +1,10 @@
 package service
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Option struct {
 	Code        string `json:"code"`
 	Description string `json:"description"`
@@ -18,6 +23,37 @@ var questions = []Question{
 		Options: []Option{{Code: "1", Description: "3"}, {Code: "2", Description: "6"}, {Code: "3", Description: "9"}}},
 }
 
+var questionsAnswers = map[string]string{
+	"1": "2",
+	"2": "2",
+}
+
 func GetAll() []Question {
 	return questions
+}
+
+func Create(user string, answers map[string]string) {
+
+	// calc score
+	score := 0
+	for key, value := range answers {
+		if questionsAnswers[key] == value {
+			score++
+		}
+	}
+
+	// save on database
+	// user, score, answers
+
+	jsonData, err := json.Marshal(answers)
+	if err != nil {
+		fmt.Println("Erro ao serializar em JSON:", err)
+		return
+	}
+
+	jsonString := string(jsonData)
+
+	fmt.Println("user = ", user)
+	fmt.Println("score = ", score)
+	fmt.Println("data = ", jsonString)
 }
