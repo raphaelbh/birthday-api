a6aa528f857b2a43dce952a3d3290894f34accd8
Raphael Oliveira <raphaeldias.ti@gmail.com>
2023-08-02 16:08:08 -0300

    feat: create message endpoint

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
+	messages "github.com/raphaelbh/birthday-api/messages"
 	questions "github.com/raphaelbh/birthday-api/questions"
 	quiz "github.com/raphaelbh/birthday-api/quiz"
 )
@@ -25,6 +26,8 @@ func main() {
 	router.GET("/questions", getQuestions)
 	router.POST("/quiz", postQuiz)
 	router.GET("/rank", getRank)
+
+	router.POST("/messages", postMessage)
 
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
@@ -56,4 +59,15 @@ func postQuiz(c *gin.Context) {
 
 	quiz.Create(user, answers)
 	c.IndentedJSON(http.StatusCreated, answers)
+}
+
+func postMessage(c *gin.Context) {
+	var message messages.Message
+
+	if err := c.BindJSON(&message); err != nil {
+		return
+	}
+
+	messages.Create(message)
+	c.IndentedJSON(http.StatusCreated, message)
 }
---
+++ messages/service.go
@@ -0,0 +1,31 @@
+package messages
+
+import (
+	"fmt"
+
+	"github.com/raphaelbh/birthday-api/database"
+	"gorm.io/gorm"
+)
+
+var db = database.Connect()
+
+type Message struct {
+	gorm.Model
+	User    string `json:"user"`
+	Message string `json:"message" gorm:"type:text"`
+}
+
+func Create(message Message) {
+
+	err := db.AutoMigrate(&Message{})
+	if err != nil {
+		panic("Error in table creation: " + err.Error())
+	}
+
+	result := db.Create(&message)
+	if result.Error != nil {
+		panic("Erro saving message: " + result.Error.Error())
+	}
+
+	fmt.Println("Message saved")
+}
